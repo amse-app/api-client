48dc60a3f3017120cdab743ef54e201a8affbfb7
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2022-06-01 17:31:20 +0200

    run dart create -t package

---
+++ lib/api_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library api_client;
+
+export 'src/api_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
