30df179ce646f3d0d55a9d34ae2febea981ffbbd
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2022-06-14 16:50:17 +0200

    add models lib

---
+++ lib/models.dart
@@ -0,0 +1,9 @@
+export 'src/competitions/models/comp.dart'
+    show Comp, CompAssign, CompAssignCriteria, CompConfig, CompScoring;
+export 'src/competitions/models/min_comp.dart' show MinComp;
+
+export 'src/event/models/event.dart' show Event;
+
+export 'src/participants/models/participant.dart'
+    show Participant, ParticipantCompetition;
+export 'src/participants/models/min_participant.dart' show MinParticipant;
